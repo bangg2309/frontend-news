c7882f890866db4a0cb4a24cd6bee9d4f106225e
MinhTan <21130171@st.hcmuaf.edu.vn>
2024-05-13 12:12:43 +0700

    finish header

---
+++ news/src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faFax, faHouse, faLocationDot, faPhone, faShareNodes} from "@fortawesome/free-solid-svg-icons";
+import './footer.css';
+const Header: React.FC = () => {
+    return(
+        <
+    )
+}
