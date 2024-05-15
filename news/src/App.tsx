01af9fcd1cb52e692e3844fbd1482506dae5d90d
Minh Chien <21130010@st.hcmuaf.edu.vn>
2024-05-15 11:48:08 +0700

    revert app.tsx component

--- news/src/App.tsx
+++ news/src/App.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/Home';
-import Header from "./components/header/Header";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
 
 function App() {
     return (
