bf5007a68067fcaee51ac1285ec4df39d55873f1
MinhTan <21130171@st.hcmuaf.edu.vn>
2024-07-03 22:19:21 +0700

    fix using lib

--- news/src/components/post/TitlePage.tsx
+++ news/src/components/post/TitlePage.tsx
@@ -1,39 +1,42 @@
+import {motion} from 'framer-motion';
 import React, {useContext} from 'react';
 import TextLink from "../text/link";
+import {Box, Typography} from "@mui/material";
 // import theme from "../../theme/theme";
 const TitlePage: React.FC<{ title: string, size?: string }> = (props) => {
     return (
-        <div className="title-page">
-            <div className="container font-bold">
-                {/*<ThemeProvider theme={theme}>*/}
-                <h2 className="title-page__title">
-                    <div style={activeStyle(props.size)} >
-                        <TextLink context={props.title.toUpperCase()}/>
-                    </div>
-                </h2>
-                {/*</ThemeProvider>*/}
-            </div>
-        </div>
-    )
+        <h2 className="title-page__title">
+            <Box>
+                <Typography variant="h3" sx={{fontSize: props.size}}>
+                    <motion.a
+                        href="#"
+                        whileHover={{color: '#2d67ad'}}
+                    >
+                       {props.title}
+                    </motion.a>
+                </Typography>
+            </Box>
+        </h2>
+)
 }
 //active status for the link
-export const activeStyle = (size?: string) => {
-    let result = {
-        //set font size with size props}
-        fontSize: "16px",
-        lineHeight: "20px"
-    }
-    if (size === "lg") {
-        result = {
-            ...result,
-            fontSize: "20px"
-        }
-    } else if (size === "md") {
-        result = {
-            ...result,
-            fontSize: "`8px"
-        }
-    }
-    return result;
-}
+// export const activeStyle = (size?: string) => {
+//     let result = {
+//         //set font size with size props}
+//         fontSize: "16px",
+//         lineHeight: "20px"
+//     }
+//     if (size === "lg") {
+//         result = {
+//             ...result,
+//             fontSize: "20px"
+//         }
+//     } else if (size === "md") {
+//         result = {
+//             ...result,
+//             fontSize: "`8px"
+//         }
+//     }
+//     return result;
+// }
 export default TitlePage;
