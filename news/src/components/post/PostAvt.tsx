b07dd5f6cdddbc23868fcc9485448e229608fba1
Minh Chien <21130010@st.hcmuaf.edu.vn>
2024-07-17 19:45:32 +0700

    fix size and rss of component in homecontent

--- news/src/components/post/PostAvt.tsx
+++ news/src/components/post/PostAvt.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const postAvt: React.FC<{ src: string; style?: React.CSSProperties }> = ({ src, style }) => {
     return (
         <div className="post-avt" style={style}>
-            <img src={src} alt="Image" style={{ width: '100%'}} />
+            <img src={src} alt="Image" style={{ width: '100%', maxWidth:'130%'}} />
         </div>
     );
 }
--- news/src/components/top-story/BigSection.tsx
+++ news/src/components/top-story/BigSection.tsx
@@ -2,24 +2,33 @@ import React from "react";
 import {RSSItem} from "../../interfaces/RSSItem";
 import {Grid, Stack} from "@mui/material";
 import SectionTopStory2th from "./SectionTopStory2th";
-import Breedcrumb from "../breedcrumb/breedcrumb";
-import {Category} from "../../interfaces/Category";
+const getRandomUniqueSlice = (array: RSSItem[], count: number, maxIndex: number) => {
+    const indices = new Set<number>();
+    while (indices.size < count && indices.size < array.length) {
+        const randomIndex = Math.floor(Math.random() * maxIndex);
+        if (randomIndex < array.length) {
+            indices.add(randomIndex);
+        }
+    }
+    return Array.from(indices).map(index => array[index]);
+}
 
-const BigSection: React.FC<{ item: RSSItem[] }> = ({item}) => {
-    const bigPost = item.slice(0, 1);
-    const list2th = item.slice(1, 3);
-    const list3th = item.slice(3, 5);
-    const list6th = item.slice(6, 11);
+const BigSection: React.FC<{ item: RSSItem[] }> = ({ item }) => {
+    const maxIndex = 20;
+    const bigPost = getRandomUniqueSlice(item, 1, maxIndex);
+    const list2th = getRandomUniqueSlice(item, 2, maxIndex);
+    const list3th = getRandomUniqueSlice(item, 2, maxIndex);
+    const list6th = getRandomUniqueSlice(item, 5, maxIndex);
 
     return (
         <>
-            {/*<Breedcrumb category={}/>*/}
+            {/*<Breedcrumb category={Category}/>*/}
             <Stack direction="column" spacing={2}>
                 <Stack direction="row" spacing={2}>
-                    <Grid item xs={6} style={{display:"contents"}}>
+                    <Grid item xs={8} style={{display:"contents"}}>
                         <Stack>
                             {bigPost.map((item, index) => (
-                                <SectionTopStory2th item={item} key={index}/>
+                                <SectionTopStory2th item={item} key={index} style={{width:'-webkit-fill-available'}}/>
                             ))}
                         </Stack>
                     </Grid>
